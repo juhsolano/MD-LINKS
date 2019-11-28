8245be3ca122f08ee5404eccb225a5207c188033
Juanita <juanita.hsolano@gamil.com>
2019-11-28 14:14:37 -0300

    linha de comando e pegando link

--- cli.js
+++ cli.js
@@ -1,3 +1,8 @@
+#!/usr/bin/env node 
+
 const { mdLinks } = require ('./lib/index.js');
 const path = process.argv.slice(2).pop();
-mdLinks(path);
+
+mdLinks(path)
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
